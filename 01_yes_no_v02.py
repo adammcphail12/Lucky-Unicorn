dbc1bc14480cb5da8ed3fa2f0535ec48e4af299a
adammcphail12 <99302328+adammcphail12@users.noreply.github.com>
2022-02-15 18:36:52 +1300

    Add files via upload
    Final YesNo/Valid Answer Program

---
+++ 01_yes_no_v02.py
@@ -0,0 +1,66 @@
+
+# Intro sequence
+print("Hello, Welcome to my Game.")
+
+#sets input and checks if its valid
+Input = input("Type in your answer below and then press enter\n(Acceptable answers include y, yes, no, n) \nHave you played before? ")
+
+
+
+
+
+valid_answers = ["yes", "y","YES", "Yes", "no", "n", "NO","No",]
+
+    
+while Input not in valid_answers:
+    print("Your answer was not valid \nPlease try again")
+    Input = input("Type in your answer below and then press enter\n(Acceptable answers include y, yes, no, n): ")
+
+#Variables
+noAnswers = ["no", "n" "No" "NO"]
+yesAnswers = ["yes", "Yes", "y", "YES"]
+noTrue = "false"
+
+#Rules
+if Input in noAnswers:
+    noTrue = "true"
+    while noTrue == "true":
+     input("Rules TBC  \n(Press Enter When Ready)")
+     Input = input("Would you like to read again?: ")
+     if Input in noAnswers:
+         noTrue = "false"
+
+
+
+print("\nCoolio, Lets Get Started...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
