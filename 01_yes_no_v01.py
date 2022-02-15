80c4acea5b9f4419f9463b1cd860f3038ba5845e
adammcphail12 <99302328+adammcphail12@users.noreply.github.com>
2022-02-15 18:36:01 +1300

    Add files via upload
    yes no for instructions and validation version 01

---
+++ 01_yes_no_v01.py
@@ -0,0 +1,16 @@
+print("Hello, Welcome to my Game.")
+print("Have you played before?")
+yesnochecker = input("").lower()
+
+yesno = 
+
+noawnserchecker = ["n", "no"]
+yesawnsersawnsers = ["yes", "y"]
+if yesnochecker in yesawnsersawnsers:
+    print("you said yes")
+    yesno == "yes"
+if yesnochecker in noawnserchecker:
+    yesno == "no"
+else:
+     print("Valid awnsers include ")
+    
